544381dc6e4ab3a147f254fbf45af6c41a4292a5
Sasa Savic Perusina <sasa@bubulazy.com>
2023-09-26 12:27:51 +0100

    Creado el componente Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Header from "@/app/componentes/Header";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h2>Bubulazy.com en ñ y é y ščž</h2>
-    </main>
+    <>
+      <Header />
+      <main>
+        <h2>Bubulazy.com en ñ y é y ščž</h2>
+      </main>
+    </>
   );
 }
