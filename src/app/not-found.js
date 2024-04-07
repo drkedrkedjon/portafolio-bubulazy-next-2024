54da23d880e59440a0fa4b1388c6c5170d7f0f6e
Sasa Savic Perusina <sasa@bubulazy.com>
2024-04-07 14:48:35 +0100

    Added 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,8 @@
+import "@/app/globals.css";
+
 export default function NotFound() {
   return (
-    <main>
+    <main className="error404">
       <h1>404: Not Found</h1>
       <p>Lo siento, la pagina que busca no existe.</p>
     </main>
