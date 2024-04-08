3ae794a3d2ef06e036d551d39592ce4f197c755b
Sasa Savic Perusina <sasa@bubulazy.com>
2024-04-08 22:03:12 +0100

    5th try to fix a vercel bug 404...

--- src/app/utilities/node-helpers/node-fs-helpers.js
+++ src/app/utilities/node-helpers/node-fs-helpers.js
@@ -7,6 +7,8 @@ function readFile(localPath) {
   return fs.readFile(path.join(process.cwd(), localPath), "utf8");
 }
 
+const rawContent = await readFile(`/blog-mdx-files/${slug}.mdx`);
+
 function readDirectory(localPath) {
   return fs.readdir(path.join(process.cwd(), localPath));
 }
@@ -28,22 +30,22 @@ export async function getBlogPostList() {
   return blogPosts.sort((p1, p2) => (p1.publishedOn > p2.publishedOn ? 1 : -1));
 }
 
-export async function loadBlogPost(slug) {
-  const filePath = path.join(process.cwd(), "/blog-mdx-files/", `${slug}.mdx`);
-  const rawContent = await fs.readFile(filePath, "utf8");
-
-  const { data: frontmatter, content } = matter(rawContent);
-
-  return { frontmatter, content };
-}
-
 // export async function loadBlogPost(slug) {
-//   const rawContent = await readFile(`/blog-mdx-files/${slug}.mdx`);
+//   const filePath = path.join(process.cwd(), "/blog-mdx-files/", `${slug}.mdx`);
+//   const rawContent = await fs.readFile(filePath, "utf8");
 
 //   const { data: frontmatter, content } = matter(rawContent);
 
 //   return { frontmatter, content };
 // }
+
+export async function loadBlogPost(slug) {
+  const rawContent = await readFile(`blog-mdx-files/${slug}.mdx`);
+
+  const { data: frontmatter, content } = matter(rawContent);
+
+  return { frontmatter, content };
+}
 
 // export const loadBlogPost = React.cache(async function loadBlogPost(slug) {
 //   let rawContent;
