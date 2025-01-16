9aebc1eae9de7f16a172cabdf7ebb23f9b96bea9
Sasha S.P. <sasa@bubulazy.com>
2025-01-16 13:26:20 +0000

    Working on images for Zorka

--- src/app/[locale]/utilities/constants.js
+++ src/app/[locale]/utilities/constants.js
@@ -2,4 +2,5 @@ export const BASE_METADATA = {
   title: "Home of Bubulazy",
   description:
     "Portafolio y blog personal donde comparto mis proyectos de desarrollo FrontEnd y también borradores sobre algunas que otras temas que me interesan...",
+  charset: "UTF-8",
 };
