fc7d21b42b4e9019814d197d6942d103fda4fa4c
Sasha S.P. <sasa@bubulazy.com>
2025-09-02 16:27:10 +0100

    Work on pagination in progres

--- src/app/[locale]/componentes/DraftsPaginationList/DraftsPaginationList.jsx
+++ src/app/[locale]/componentes/DraftsPaginationList/DraftsPaginationList.jsx
@@ -2,15 +2,15 @@
 import styles from "./DraftsPaginationList.module.css";
 import { useState } from "react";
 import DraftsCard from "../DraftsCard";
+import { ArrowLeftCircle, ArrowRightCircle } from "react-feather";
 
 export default function DraftsPaginationList({ drafts }) {
   const [pageNumber, setPageNumber] = useState(3);
-  const [draftsPerPage, setDraftsPerPage] = useState(3);
+  const [draftsPerPage, setDraftsPerPage] = useState(2);
 
   const initialSlice = (pageNumber - 1) * draftsPerPage;
   const finalSlice = initialSlice + draftsPerPage;
   const pageContent = drafts.slice(initialSlice, finalSlice);
-
   const numberOfPages = Math.ceil(drafts.length / draftsPerPage);
 
   return (
@@ -23,14 +23,31 @@ export default function DraftsPaginationList({ drafts }) {
       ))}
       <div className={styles.controlsContainer}>
         <div className={styles.prevNextContainer}>
-          <button>Last</button>
-          <button>Next</button>
-          <button>Resset</button>
+          <button>
+            <ArrowLeftCircle />
+          </button>
+          <div>{pageNumber}</div>
+          <button>
+            <ArrowRightCircle />
+          </button>
         </div>
-        <div className={styles.pagerContainer}>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+        <div className={styles.rangeContainer}>
+          <input
+            type="range"
+            className={styles.range}
+            min="1"
+            max={numberOfPages}
+            step="1"
+            value={pageNumber}
+            onChange={(e) => setPageNumber(Number(e.target.value))}
+          />
+          <div className={styles.ticksContainer}>
+            <span className={styles.tick}>1</span>
+            <span className={styles.tick}>2</span>
+            <span className={styles.tick}>3</span>
+            <span className={styles.tick}>4</span>
+            <span className={styles.tick}>5</span>
+          </div>
         </div>
       </div>
     </>
